cb5552fea828b89c183a4001b641874b724fdcd9
nkoexe <niccolo.rag@gmail.com>
2023-11-25 23:05:35 +0100

    socketiofix

---
+++ sostituzioni/__main__.py
@@ -0,0 +1,10 @@
+"""
+durante la fase di sviluppo eseguire questo file tramite python -m sostituzioni
+"""
+
+from sostituzioni.app import app
+from sostituzioni.view import socketio
+
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
--- sostituzioni/app.py
+++ sostituzioni/app.py
@@ -1,7 +1,1 @@
-"""
-eseguire questo file!!!
-tramite flask run!!!
-fare cd in questa cartella ed eseguire flask run --debug
-"""
-
 from sostituzioni.view import app
