d798e67f0221ee40af33a881f6dd7015fd74f323
nkoexe <niccolo.rag@gmail.com>
2023-12-26 14:39:28 +0100

    la shell è andata :'(

--- sostituzioni/view/impostazioni/routes.py
+++ sostituzioni/view/impostazioni/routes.py
@@ -1,18 +1,12 @@
 from flask import render_template
 
 from sostituzioni.control.configurazione import configurazione
-from sostituzioni.model.auth import login_required, has_role, current_user
+from sostituzioni.model.auth import login_required, role_required
 from sostituzioni.view.impostazioni import impostazioni
 
 
 @impostazioni.route('/impostazioni')
 @login_required
-@has_role(current_user.permessi.impostazioni.write)
+@role_required('impostazioni.write')
 def main():
     return render_template('impostazioni.html', configurazione=configurazione)
-
-
-@impostazioni.route('/shell')
-@login_required
-def shell():
-    return render_template('shell.html')
