153a7ab8d18d2ab9083e62e6d35f12ec9967b79a
nkoexe <niccolo.rag@gmail.com>
2024-01-04 11:15:53 +0100

    utenti

--- sostituzioni/view/impostazioni/routes.py
+++ sostituzioni/view/impostazioni/routes.py
@@ -1,6 +1,8 @@
 from flask import render_template
 
 from sostituzioni.control.configurazione import configurazione
+from sostituzioni.control.database import utenti
+from sostituzioni.model.model import Docente
 from sostituzioni.model.auth import login_required, role_required
 from sostituzioni.view.impostazioni import impostazioni
 
@@ -9,4 +11,9 @@ from sostituzioni.view.impostazioni import impostazioni
 @login_required
 @role_required("impostazioni.write")
 def main():
-    return render_template("impostazioni.html", configurazione=configurazione)
+    return render_template(
+        "impostazioni.html",
+        configurazione=configurazione,
+        utenti=utenti,
+        docenti=Docente.load(),
+    )
