41118f36ded674338a96e19acf22723005407e0f
nkoexe <niccolo.rag@gmail.com>
2024-01-10 00:58:42 +0100

    cookie duration

--- sostituzioni/model/app.py
+++ sostituzioni/model/app.py
@@ -11,10 +11,14 @@ app.static_folder = configurazione.get("flaskstaticdir").path
 app.template_folder = configurazione.get("flasktemplatedir").path
 
 app.config["SECRET_KEY"] = token_hex(32)
-app.config["SESSION_COOKIE_SECURE"] = True
 app.config["SESSION_COOKIE_HTTPONLY"] = True
+app.config["SESSION_COOKIE_SECURE"] = True
 app.config["SESSION_COOKIE_SAMESITE"] = "Lax"
-
+# sessiona dura 1 ora, dopodichè bisogna eseguire nuovamente il login
+app.config["PERMANENT_SESSION_LIFETIME"] = 60 * 60
+# il countdown viene ripristinato ad ogni richiesta
+# ! attenzione, non si applica a socketio, soltanto a richieste http
+app.config["SESSION_REFRESH_EACH_REQUEST"] = True
 
 app.cli.add_command(database_cli)
 app.cli.add_command(importer_cli)
