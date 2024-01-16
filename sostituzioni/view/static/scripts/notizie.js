a9e6582a67805f8ddeb33a2074ebb0e1d58b805d
nkoexe <niccolo.rag@gmail.com>
2024-01-17 00:08:48 +0100

    notizie future

--- sostituzioni/view/static/scripts/notizie.js
+++ sostituzioni/view/static/scripts/notizie.js
@@ -1,6 +1,6 @@
 const ui_notizia_html_template = `
 <li>
-<div class="notizia" data-id={id} tabindex="0">
+<div class="notizia {futura}" data-id={id} tabindex="0">
     <div class="notizia-testo">{testo}</div>
 </div>
 </li>`
@@ -9,12 +9,14 @@ const ui_notizia_html_template = `
 const ui_notizie_container = document.getElementById("notizie-container")
 const ui_notizie_lista = document.getElementById("notizie-lista")
 
-function format_notizia_to_html(id, pubblicato, data_ora_inizio, data_ora_fine, testo) {
-    return ui_notizia_html_template.replace("{id}", id).replace("{testo}", testo)
+function format_notizia_to_html(id, data_inizio, data_fine, testo) {
+    const futura = (data_inizio * 1000 > new Date()) ? "futura" : ""
+
+    return ui_notizia_html_template.replace("{id}", id).replace("{futura}", futura).replace("{testo}", testo)
 }
 
-function add_notizia_to_ui_list(id, pubblicato, data_ora_inizio, data_ora_fine, testo) {
-    let notizia_html = format_notizia_to_html(id, pubblicato, data_ora_inizio, data_ora_fine, testo)
+function add_notizia_to_ui_list(id, data_inizio, data_fine, testo) {
+    let notizia_html = format_notizia_to_html(id, data_inizio, data_fine, testo)
     ui_notizie_lista.innerHTML += notizia_html
 }
 
@@ -27,7 +29,7 @@ function ui_modifica_notizia() {
 function refresh_notizie() {
     ui_notizie_lista.innerHTML = ""
     notizie.forEach(element => {
-        add_notizia_to_ui_list(element.id, element.pubblicato, element.data_ora_inizio, element.data_ora_fine, element.testo)
+        add_notizia_to_ui_list(element.id, element.data_inizio, element.data_fine, element.testo)
     })
 
     if (notizie_write) {
