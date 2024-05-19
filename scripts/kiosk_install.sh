bba4c3e5b3519b98d672f9126d61bd9c08300785
nkoexe <niccolo.rag@gmail.com>
2024-05-19 22:05:01 +0200

    test display script

--- scripts/kiosk_install.sh
+++ scripts/kiosk_install.sh
@@ -37,6 +37,35 @@ autologin-user=scuolasync
 user-session=openbox
 EOF
 
+cat > /etc/lightdm/display.sh << EOF
+#!/bin/bash
+
+# Get the names of connected displays
+displays=\$(xrandr | grep " connected" | cut -d" " -f1)
+
+# Find the biggest resolution available among all connected displays
+biggest_resolution=\$(xrandr | grep -oP '\d+x\d+(?=\s+\d+.\d+\s+\d+\.\d+\*)' | sort -nr | head -n 1)
+
+if [ -z "\$biggest_resolution" ]; then
+  echo "No resolutions found."
+  exit 1
+fi
+
+# Set the resolution for all displays
+for display in \$displays; do
+  xrandr --output \$display --mode \$biggest_resolution
+done
+
+# Mirror all displays to the primary display
+primary_display=\$(echo \$displays | head -n 1)
+for display in \$displays; do
+  if [ "\$display" != "\$primary_display" ]; then
+    xrandr --output \$display --same-as \$primary_display
+  fi
+done
+EOF
+
+
 # input da utente del sito web
 read -p "Inserire l'url di base del sito (senza https://): " url
 # input da utente per il codice di autorizzazione
