5dd813fa64104a732b22f3fedd25d022bd26644d
nkoexe <niccolo.rag@gmail.com>
2024-05-26 11:56:57 +0200

    deleted unused publish script

--- scripts/publish.sh
+++
@@ -1,3 +0,0 @@
-# todo impostare sistema in manutenzione, controllare file (login non sia bypassato), upload con scp, reboot sistema
-
-scp -i ~/.ssh/ssh_key_gandhi -P 8484 -r ~/Proj/sostituzioni/sostituzioni root@gandhi-merano.fuss.bz.it:/sostituzioni/sostituzioni
