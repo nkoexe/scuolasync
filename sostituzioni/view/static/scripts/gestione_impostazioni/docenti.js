f6665d0cc6c1e27447cdbe8ee1e31d0a177d3c08
nkoexe <niccolo.rag@gmail.com>
2024-06-09 19:10:34 +0200

    ricerca di docenti per nome e cognome

--- sostituzioni/view/static/scripts/gestione_impostazioni/docenti.js
+++ sostituzioni/view/static/scripts/gestione_impostazioni/docenti.js
@@ -7,7 +7,10 @@ const docente_template = `<div class="nome-docente">
 <button class="material-symbols-rounded pulsante-conferma-modifiche-dato hidden">check_circle</button>
 </div>`
 
+const ui_cerca_docente = document.querySelector("#cerca-docente-input")
 const ui_lista_docenti = document.querySelector("#lista-docenti")
+
+let lista_elementi = []
 
 function crea_elemento(nome, cognome) {
   const element = document.createElement("div")
@@ -37,22 +40,45 @@ docenti.sort((a, b) => {
   return a[1].localeCompare(b[1])
 })
 
+// function genera_lista_docenti(docenti) {
 docenti.forEach(docenti => {
   const element = crea_elemento(docenti[0], docenti[1])
   ui_lista_docenti.appendChild(element)
 })
+// }
+
+lista_elementi = Array.from(document.querySelectorAll(".opzione-docente"))
+
+function cerca_docente() {
+  const query = document.querySelector("#cerca-docente-input").value.toLowerCase()
+
+  if (query == "") {
+    lista_elementi.forEach(element => {
+      element.classList.remove("hidden")
+    })
+    return
+  }
+
+  lista_elementi.forEach(element => {
+    const nome = element.dataset.nome.toLowerCase()
+    const cognome = element.dataset.cognome.toLowerCase()
+    if ((nome + " " + cognome).includes(query) || (cognome + " " + nome).includes(query)) {
+      element.classList.remove("hidden")
+    } else {
+      element.classList.add("hidden")
+    }
+  })
+}
 
 function modificato(nome, cognome) {
-  const element = Array.from(document.querySelectorAll(".opzione-docente"))
-    .find(element => element.dataset.nome === nome && element.dataset.cognome === cognome)
+  const element = lista_elementi.find(element => element.dataset.nome === nome && element.dataset.cognome === cognome)
 
   element.querySelector(".pulsante-elimina-dato").classList.add("hidden")
   element.querySelector(".pulsante-conferma-modifiche-dato").classList.remove("hidden")
 }
 
 function ui_conferma_modifiche(nome, cognome) {
-  const element = Array.from(document.querySelectorAll(".opzione-docente"))
-    .find(element => element.dataset.nome === nome && element.dataset.cognome === cognome)
+  const element = lista_elementi.find(element => element.dataset.nome === nome && element.dataset.cognome === cognome)
 
   let new_nome = element.querySelector(".input-nome-docente").value
   let new_cognome = element.querySelector(".input-cognome-docente").value
@@ -85,9 +111,9 @@ function ui_conferma_modifiche(nome, cognome) {
 
 function ui_elimina_docente(nome, cognome) {
   if (nome == "" || cognome == "") {
-    const element = Array.from(document.querySelectorAll(".opzione-docente"))
-      .find(element => element.dataset.nome === nome && element.dataset.cognome === cognome)
+    const element = lista_elementi.find(element => element.dataset.nome === nome && element.dataset.cognome === cognome)
     element.remove()
+    lista_elementi.splice(lista_elementi.indexOf(element), 1)
     return
   }
   mostra_popup_conferma({ titolo: "Elimina Docente", descrizione: "Sei sicuro di voler eliminare il docente <strong>" + nome + " " + cognome + "</strong>?", testo_pulsante_secondario: "Annulla", testo_pulsante_primario: "Elimina", callback: () => { elimina_docente(nome, cognome) } })
@@ -108,6 +134,7 @@ function nuovo_docente() {
   }
 
   const empty_element = crea_elemento("", "")
+  lista_elementi.push(empty_element)
   ui_lista_docenti.insertAdjacentElement("afterbegin", empty_element)
   empty_element.querySelector(".input-cognome-docente").focus()
 }
@@ -116,11 +143,12 @@ function nuovo_docente() {
 
 
 socket.on("modifica docente successo", (data) => {
-  const element = Array.from(document.querySelectorAll(".opzione-docente"))
-    .find(element => element.dataset.nome === data.nome && element.dataset.cognome === data.cognome)
+  const element = lista_elementi.find(element => element.dataset.nome === data.nome && element.dataset.cognome === data.cognome)
   const new_element = crea_elemento(data.new_nome, data.new_cognome)
 
   element.replaceWith(new_element)
+  lista_elementi.splice(lista_elementi.indexOf(element), 1)
+  lista_elementi.push(new_element)
 
   if (data.nome == "" || data.cognome == "") {
     // nuovo docente
@@ -140,8 +168,7 @@ socket.on("modifica docente successo", (data) => {
 })
 
 socket.on("modifica docente errore", (data) => {
-  const element = Array.from(document.querySelectorAll(".opzione-docente"))
-    .find(element => element.dataset.nome === data.nome && element.dataset.cognome === data.cognome)
+  const element = lista_elementi.find(element => element.dataset.nome === data.nome && element.dataset.cognome === data.cognome)
   element.querySelector(".input-nome-docente").disabled = false
   element.querySelector(".input-cognome-docente").disabled = false
 
@@ -157,9 +184,9 @@ socket.on("elimina docente successo", (data) => {
   if (index == -1) return;
   docenti.splice(index, 1)
 
-  const element = Array.from(document.querySelectorAll(".opzione-docente"))
-    .find(element => element.dataset.nome === data.nome && element.dataset.cognome === data.cognome)
+  const element = lista_elementi.find(element => element.dataset.nome === data.nome && element.dataset.cognome === data.cognome)
   element.remove()
+  lista_elementi.splice(lista_elementi.indexOf(element), 1)
 
   notyf.success("Docente eliminato con successo")
 })
