bb4476894204c75d565d347938ee1602c68e8223
nkoexe <niccolo.rag@gmail.com>
2024-07-17 17:24:27 +0200

    experimenting with the view transitions api

--- sostituzioni/view/static/scripts/impostazioni.js
+++ sostituzioni/view/static/scripts/impostazioni.js
@@ -8,16 +8,16 @@ let modifiche = []
 let sezioni = document.getElementsByClassName('sezione-header');
 
 for (let index = 0; index < sezioni.length; index++) {
-    sezioni[index].addEventListener('click', () => {
-        sezioni[index].classList.toggle('expanded');
+  sezioni[index].addEventListener('click', () => {
+    sezioni[index].classList.toggle('expanded');
 
-        let listaopzioni = sezioni[index].nextElementSibling;
-        if (listaopzioni.style.maxHeight) {
-            listaopzioni.style.maxHeight = null;
-        } else {
-            listaopzioni.style.maxHeight = listaopzioni.scrollHeight + 'px';
-        }
-    });
+    let listaopzioni = sezioni[index].nextElementSibling;
+    if (listaopzioni.style.maxHeight) {
+      listaopzioni.style.maxHeight = null;
+    } else {
+      listaopzioni.style.maxHeight = listaopzioni.scrollHeight + 'px';
+    }
+  });
 }
 
 
@@ -53,75 +53,95 @@ for (let index = 0; index < sezioni.length; index++) {
 
 
 function modificato(id) {
-    if (!modifiche.includes(id)) {
-        modifiche.push(id);
-        mostra_pulsante_applica();
-    }
+  if (!modifiche.includes(id)) {
+    modifiche.push(id);
+    mostra_pulsante_applica();
+  }
 }
 
 
 function mostra_pulsante_applica() {
-    pulsante_applica.disabled = false;
+  pulsante_applica.disabled = false;
 }
 
 function applica() {
-    // Da mandare al server, {id_opzione: valore, "id2": ["12", "px"]} 
-    let dati = {};
+  // Da mandare al server, {id_opzione: valore, "id2": ["12", "px"]} 
+  let dati = {};
 
-    modifiche.forEach(element => {
+  modifiche.forEach(element => {
 
-        if (element.classList.contains("opzione-testo")) {
-            // Se l'opzione è un testo
+    if (element.classList.contains("opzione-testo")) {
+      // Se l'opzione è un testo
 
-            let testo = element.getElementsByTagName("input")[0];
-            dati[element.id] = testo.value;
+      let testo = element.getElementsByTagName("input")[0];
+      dati[element.id] = testo.value;
 
-        } else if (element.classList.contains("opzione-numero")) {
-            // Se l'opzione è un numero
+    } else if (element.classList.contains("opzione-numero")) {
+      // Se l'opzione è un numero
 
-            let numero = element.getElementsByTagName("input")[0];
-            dati[element.id] = numero.valueAsNumber;
+      let numero = element.getElementsByTagName("input")[0];
+      dati[element.id] = numero.valueAsNumber;
 
-        } else if (element.classList.contains("opzione-numero-unita")) {
-            // Se l'opzione è un numero con unità
+    } else if (element.classList.contains("opzione-numero-unita")) {
+      // Se l'opzione è un numero con unità
 
-            let valore = element.getElementsByTagName("input")[0];
-            let unita = element.getElementsByTagName("select")[0];
-            dati[element.id] = [valore.valueAsNumber, unita.selectedIndex]
+      let valore = element.getElementsByTagName("input")[0];
+      let unita = element.getElementsByTagName("select")[0];
+      dati[element.id] = [valore.valueAsNumber, unita.selectedIndex]
 
-        } else if (element.classList.contains("opzione-booleano")) {
-            // Se l'opzione è un checkbox
+    } else if (element.classList.contains("opzione-booleano")) {
+      // Se l'opzione è un checkbox
 
-            let checkbox = element.getElementsByTagName("input")[0];
-            dati[element.id] = checkbox.checked;
+      let checkbox = element.getElementsByTagName("input")[0];
+      dati[element.id] = checkbox.checked;
 
-        }
-        else if (element.classList.contains("opzione-selezione")) {
-            // Se l'opzione è un dropdown
+    }
+    else if (element.classList.contains("opzione-selezione")) {
+      // Se l'opzione è un dropdown
 
-            let select = element.getElementsByTagName("select")[0];
-            dati[element.id] = select.selectedIndex;
+      let select = element.getElementsByTagName("select")[0];
+      dati[element.id] = select.selectedIndex;
 
-        } else if (element.classList.contains("opzione-colore")) {
-            // Se l'opzione è un colorpicker
+    } else if (element.classList.contains("opzione-colore")) {
+      // Se l'opzione è un colorpicker
 
-        } else if (element.classList.contains("opzione-percorso")) {
-            // Se l'opzione è un percorso
+    } else if (element.classList.contains("opzione-percorso")) {
+      // Se l'opzione è un percorso
 
-            let radice = element.getElementsByTagName("select")[0];
-            let percorso = element.getElementsByTagName("input")[0];
-            dati[element.id] = [radice.selectedIndex, percorso.value]
+      let radice = element.getElementsByTagName("select")[0];
+      let percorso = element.getElementsByTagName("input")[0];
+      dati[element.id] = [radice.selectedIndex, percorso.value]
 
-        }
-    })
+    }
+  })
 
-    socket.emit("applica impostazioni", dati);
+  socket.emit("applica impostazioni", dati);
 }
 
 socket.on("applica impostazioni errore", (errore) => {
-    alert(errore);
+  alert(errore);
 })
 
 socket.on("applica impostazioni successo", (data) => {
-    location.reload();
+  location.reload();
 })
+
+window.addEventListener('pageswap', async (e) => {
+  if (e.viewTransition) {
+    const targetUrl = new URL(e.activation.entry.url);
+
+    if (targetUrl.pathname.startsWith('/impostazioni')) {
+      const element = document.querySelector('.link-gestione:focus');
+      const elementicon = element.querySelector('.icon');
+      const elementtext = element.querySelector('h2');
+
+      element.style.viewTransitionName = 'gestione-dati';
+      elementicon.style.viewTransitionName = 'gestione-dati-icon';
+      elementtext.style.viewTransitionName = 'gestione-dati-text';
+
+      // await e.viewTransition.finished;
+
+      // element.style.viewTransitionName = '';
+    }
+  }
+});
