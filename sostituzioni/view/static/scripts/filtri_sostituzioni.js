74e23bc25c5166ee357b4ab4f63cb0b2f8355f5c
nkoexe <niccolo.rag@gmail.com>
2024-09-04 15:01:10 +0200

    indentation

--- sostituzioni/view/static/scripts/filtri_sostituzioni.js
+++ sostituzioni/view/static/scripts/filtri_sostituzioni.js
@@ -1,74 +1,74 @@
 class Filtro extends Selezione {
-    constructor({ query, ordinamento, filtra_lista, render, select_on_keydown, autocomplete }) {
-        super({ query: query, callback: refresh_sostituzioni, filtra_lista: filtra_lista, render: render, select_on_keydown: select_on_keydown, autocomplete: autocomplete })
+  constructor({ query, ordinamento, filtra_lista, render, select_on_keydown, autocomplete }) {
+    super({ query: query, callback: refresh_sostituzioni, filtra_lista: filtra_lista, render: render, select_on_keydown: select_on_keydown, autocomplete: autocomplete })
 
-        this.ordinamento = ordinamento
-        this.ordinamento = (typeof this.ordinamento === "undefined") ? true : this.ordinamento
-        if (this.ordinamento) {
-            this.ui_ordinamento = this.ui_container.getElementsByClassName("sostituzioni-ordinamento")[0]
-            this.ui_ordinamento_frecciasu = this.ui_ordinamento.children[0]
-            this.ui_ordinamento_frecciagiu = this.ui_ordinamento.children[1]
+    this.ordinamento = ordinamento
+    this.ordinamento = (typeof this.ordinamento === "undefined") ? true : this.ordinamento
+    if (this.ordinamento) {
+      this.ui_ordinamento = this.ui_container.getElementsByClassName("sostituzioni-ordinamento")[0]
+      this.ui_ordinamento_frecciasu = this.ui_ordinamento.children[0]
+      this.ui_ordinamento_frecciagiu = this.ui_ordinamento.children[1]
+    }
+
+    this.ordina = false
+    this.verso_ordinamento = 1  // 1: crescente, -1: decrescente
+
+    if (this.ordinamento) {
+      this.ui_ordinamento.onclick = (event) => {
+        this.verso_ordinamento *= -1
+
+        if (this.verso_ordinamento === 1) {
+          this.ui_ordinamento_frecciasu.classList.add("selected")
+          this.ui_ordinamento_frecciagiu.classList.remove("selected")
+        } else {
+          this.ui_ordinamento_frecciasu.classList.remove("selected")
+          this.ui_ordinamento_frecciagiu.classList.add("selected")
         }
 
+        this.ordina = true
+        refresh_sostituzioni()
         this.ordina = false
-        this.verso_ordinamento = 1  // 1: crescente, -1: decrescente
-
-        if (this.ordinamento) {
-            this.ui_ordinamento.onclick = (event) => {
-                this.verso_ordinamento *= -1
-
-                if (this.verso_ordinamento === 1) {
-                    this.ui_ordinamento_frecciasu.classList.add("selected")
-                    this.ui_ordinamento_frecciagiu.classList.remove("selected")
-                } else {
-                    this.ui_ordinamento_frecciasu.classList.remove("selected")
-                    this.ui_ordinamento_frecciagiu.classList.add("selected")
-                }
-
-                this.ordina = true
-                refresh_sostituzioni()
-                this.ordina = false
-            }
-        }
+      }
     }
+  }
 
-    ui_rimuovi_ordinamento() {
-        this.ui_ordinamento_frecciasu.classList.remove("selected")
-        this.ui_ordinamento_frecciagiu.classList.remove("selected")
-    }
+  ui_rimuovi_ordinamento() {
+    this.ui_ordinamento_frecciasu.classList.remove("selected")
+    this.ui_ordinamento_frecciagiu.classList.remove("selected")
+  }
 }
 
 function sostituzioni_applica_filtri() {
-    sostituzioni_visualizzate = sostituzioni
-    if (sostituzioni_filtro_ora.selected !== null) {
-        sostituzioni_visualizzate = sostituzioni_visualizzate.filter(element => element.ora_predefinita === (sostituzioni_filtro_ora.selected))
-    }
-    if (sostituzioni_filtro_classe.selected !== null) {
-        sostituzioni_visualizzate = sostituzioni_visualizzate.filter(element => {
-            if (element.nome_classe === null) return false
-            // to lowercase and remove spaces
-            return element.nome_classe.toLowerCase().replace(/ /g, "") === sostituzioni_filtro_classe.selected.toLowerCase().replace(/ /g, "")
-        })
-    }
-    if (sostituzioni_filtro_aula.selected !== null) {
-        sostituzioni_visualizzate = sostituzioni_visualizzate.filter(element => {
-            if (element.numero_aula === null) return false
-            return element.numero_aula.toLowerCase() === sostituzioni_filtro_aula.selected.toLowerCase()
-        })
-    }
-    if (sostituzioni_filtro_docente.selected !== null) {
-        sostituzioni_visualizzate = sostituzioni_visualizzate.filter(element => {
-            if (element.cognome_docente === null) return false
-            return (element.cognome_docente + " " + element.nome_docente).toLowerCase().includes(sostituzioni_filtro_docente.selected.toLowerCase())
-        })
-    }
-    if (sostituzioni_filtro_note.selected !== null) {
-        // todo: implement fuzzy search
-        sostituzioni_visualizzate = sostituzioni_visualizzate.filter(element => {
-            if (element.note === null) return
-            return element.note.toLowerCase().includes(sostituzioni_filtro_note.selected.toLowerCase())
-        })
-    }
+  sostituzioni_visualizzate = sostituzioni
+  if (sostituzioni_filtro_ora.selected !== null) {
+    sostituzioni_visualizzate = sostituzioni_visualizzate.filter(element => element.ora_predefinita === (sostituzioni_filtro_ora.selected))
+  }
+  if (sostituzioni_filtro_classe.selected !== null) {
+    sostituzioni_visualizzate = sostituzioni_visualizzate.filter(element => {
+      if (element.nome_classe === null) return false
+      // to lowercase and remove spaces
+      return element.nome_classe.toLowerCase().replace(/ /g, "") === sostituzioni_filtro_classe.selected.toLowerCase().replace(/ /g, "")
+    })
+  }
+  if (sostituzioni_filtro_aula.selected !== null) {
+    sostituzioni_visualizzate = sostituzioni_visualizzate.filter(element => {
+      if (element.numero_aula === null) return false
+      return element.numero_aula.toLowerCase() === sostituzioni_filtro_aula.selected.toLowerCase()
+    })
+  }
+  if (sostituzioni_filtro_docente.selected !== null) {
+    sostituzioni_visualizzate = sostituzioni_visualizzate.filter(element => {
+      if (element.cognome_docente === null) return false
+      return (element.cognome_docente + " " + element.nome_docente).toLowerCase().includes(sostituzioni_filtro_docente.selected.toLowerCase())
+    })
+  }
+  if (sostituzioni_filtro_note.selected !== null) {
+    // todo: implement fuzzy search
+    sostituzioni_visualizzate = sostituzioni_visualizzate.filter(element => {
+      if (element.note === null) return
+      return element.note.toLowerCase().includes(sostituzioni_filtro_note.selected.toLowerCase())
+    })
+  }
 }
 
 let sostituzioni_filtro_ora = new Filtro({ query: "#sostituzioni-filtro-ora", filtra_lista: prendi_ora, render: element => element.length == 1 ? element + "a ora" : element, autocomplete: true })
@@ -94,185 +94,185 @@ const ui_sostituzioni_filtro_data_tutte = document.getElementById("sostituzioni-
 
 
 function sostituzioni_filtra_data() {
-    // funzione per generare i filtri con i quali caricare le sostituzioni
+  // funzione per generare i filtri con i quali caricare le sostituzioni
 
-    ui_sostituzioni_filtro_data_rimuovi_selected()
+  ui_sostituzioni_filtro_data_rimuovi_selected()
 
-    let data_inizio, data_fine, filtri
+  let data_inizio, data_fine, filtri
 
-    switch (sostituzioni_filtro_data_attivo) {
-        case "oggi":
-            data_inizio = new Date()
-            data_inizio.setHours(0, 0, 0, 0)
-            data_fine = new Date()
-            data_fine.setHours(23, 59, 59, 0)
+  switch (sostituzioni_filtro_data_attivo) {
+    case "oggi":
+      data_inizio = new Date()
+      data_inizio.setHours(0, 0, 0, 0)
+      data_fine = new Date()
+      data_fine.setHours(23, 59, 59, 0)
 
-            filtri = {
-                data_inizio: data_inizio.getTime() / 1000,
-                data_fine: data_fine.getTime() / 1000
-            }
+      filtri = {
+        data_inizio: data_inizio.getTime() / 1000,
+        data_fine: data_fine.getTime() / 1000
+      }
 
-            ui_sostituzioni_filtro_data_oggi.classList.add("selected")
-            break
+      ui_sostituzioni_filtro_data_oggi.classList.add("selected")
+      break
 
-        case "domani":
-            data_inizio = new Date()
-            data_inizio.setDate(data_inizio.getDate() + 1)
-            data_inizio.setHours(0, 0, 0, 0)
-            data_fine = new Date()
-            data_fine.setDate(data_inizio.getDate())
-            data_fine.setHours(23, 59, 59, 0)
+    case "domani":
+      data_inizio = new Date()
+      data_inizio.setDate(data_inizio.getDate() + 1)
+      data_inizio.setHours(0, 0, 0, 0)
+      data_fine = new Date()
+      data_fine.setDate(data_inizio.getDate())
+      data_fine.setHours(23, 59, 59, 0)
 
-            filtri = {
-                data_inizio: data_inizio.getTime() / 1000,
-                data_fine: data_fine.getTime() / 1000
-            }
+      filtri = {
+        data_inizio: data_inizio.getTime() / 1000,
+        data_fine: data_fine.getTime() / 1000
+      }
 
-            ui_sostituzioni_filtro_data_domani.classList.add("selected")
-            break
+      ui_sostituzioni_filtro_data_domani.classList.add("selected")
+      break
 
-        case "future":
-            data_inizio = new Date()
-            data_inizio.setHours(0, 0, 0, 0)
+    case "future":
+      data_inizio = new Date()
+      data_inizio.setHours(0, 0, 0, 0)
 
-            filtri = {
-                data_inizio: data_inizio.getTime() / 1000,
-                data_fine: null
-            }
+      filtri = {
+        data_inizio: data_inizio.getTime() / 1000,
+        data_fine: null
+      }
 
-            ui_sostituzioni_filtro_data_future.classList.add("selected")
-            break
+      ui_sostituzioni_filtro_data_future.classList.add("selected")
+      break
 
-        case "data":
-            data_inizio = ui_sostituzioni_filtro_data_data.valueAsDate
+    case "data":
+      data_inizio = ui_sostituzioni_filtro_data_data.valueAsDate
 
-            if (data_inizio == null || data_inizio == undefined) {
-                return
-            }
+      if (data_inizio == null || data_inizio == undefined) {
+        return
+      }
 
-            data_inizio.setHours(0, 0, 0, 0)
-            data_fine = new Date(data_inizio.getTime())
-            data_fine.setHours(23, 59, 59, 0)
+      data_inizio.setHours(0, 0, 0, 0)
+      data_fine = new Date(data_inizio.getTime())
+      data_fine.setHours(23, 59, 59, 0)
 
-            filtri = {
-                data_inizio: data_inizio.getTime() / 1000,
-                data_fine: data_fine.getTime() / 1000
-            }
+      filtri = {
+        data_inizio: data_inizio.getTime() / 1000,
+        data_fine: data_fine.getTime() / 1000
+      }
 
-            ui_sostituzioni_filtro_data_data.classList.add("selected")
-            break
+      ui_sostituzioni_filtro_data_data.classList.add("selected")
+      break
 
-        case "mese":
-            const mese = parseInt(ui_sostituzioni_filtro_data_mese.value)
-            const now = new Date()
-            const mese_corrente = now.getMonth() + 1
-            let anno = now.getFullYear()
-            if (mese > 7 && mese_corrente <= 7) {
-                // Anno scolastico precedente
-                anno -= 1
-            } else if (mese <= 7 && mese_corrente > 7) {
-                // Anno scolastico successivo
-                anno += 1
-            }
+    case "mese":
+      const mese = parseInt(ui_sostituzioni_filtro_data_mese.value)
+      const now = new Date()
+      const mese_corrente = now.getMonth() + 1
+      let anno = now.getFullYear()
+      if (mese > 7 && mese_corrente <= 7) {
+        // Anno scolastico precedente
+        anno -= 1
+      } else if (mese <= 7 && mese_corrente > 7) {
+        // Anno scolastico successivo
+        anno += 1
+      }
 
-            filtri = {
-                data_inizio: new Date(anno, mese, 1).getTime() / 1000,
-                data_fine: new Date(anno, mese + 1, 0, 23, 59, 59).getTime() / 1000
-            }
+      filtri = {
+        data_inizio: new Date(anno, mese, 1).getTime() / 1000,
+        data_fine: new Date(anno, mese + 1, 0, 23, 59, 59).getTime() / 1000
+      }
 
-            ui_sostituzioni_filtro_data_mese.classList.add("selected")
-            break
+      ui_sostituzioni_filtro_data_mese.classList.add("selected")
+      break
 
-        case "tutte":
-            filtri = {
-                data_inizio: 0
-            }
+    case "tutte":
+      filtri = {
+        data_inizio: 0
+      }
 
-            ui_sostituzioni_filtro_data_tutte.classList.add("selected")
-            break
+      ui_sostituzioni_filtro_data_tutte.classList.add("selected")
+      break
 
-        default:
-            data_inizio = new Date()
-            data_inizio.setHours(0, 0, 0, 0)
+    default:
+      data_inizio = new Date()
+      data_inizio.setHours(0, 0, 0, 0)
 
-            filtri = {
-                data_inizio: data_inizio.getTime() / 1000,
-                data_fine: null
-            }
+      filtri = {
+        data_inizio: data_inizio.getTime() / 1000,
+        data_fine: null
+      }
 
-            ui_sostituzioni_filtro_data_future.classList.add("selected")
-    }
+      ui_sostituzioni_filtro_data_future.classList.add("selected")
+  }
 
-    return filtri
+  return filtri
 }
 
 
 ui_sostituzioni_filtro_data_testo.onclick = (e) => {
-    if (ui_sostituzioni_filtro_data_expandible.classList.contains("active")) {
-        ui_sostituzioni_filtro_data_expandible.classList.remove("active")
-        return
-    }
+  if (ui_sostituzioni_filtro_data_expandible.classList.contains("active")) {
+    ui_sostituzioni_filtro_data_expandible.classList.remove("active")
+    return
+  }
 
-    ui_sostituzioni_filtro_data.focus()
-    ui_sostituzioni_filtro_data_expandible.classList.toggle("active")
+  ui_sostituzioni_filtro_data.focus()
+  ui_sostituzioni_filtro_data_expandible.classList.toggle("active")
 }
 
 ui_sostituzioni_filtro_data.onblur = (e) => {
-    if (e.relatedTarget && e.relatedTarget.closest("#sostituzioni-filtro-data-expandible")) {
-        e.preventDefault()
-        e.stopPropagation()
-        ui_sostituzioni_filtro_data.focus()
-        return
-    }
+  if (e.relatedTarget && e.relatedTarget.closest("#sostituzioni-filtro-data-expandible")) {
+    e.preventDefault()
+    e.stopPropagation()
+    ui_sostituzioni_filtro_data.focus()
+    return
+  }
 
-    ui_sostituzioni_filtro_data_expandible.classList.remove("active")
+  ui_sostituzioni_filtro_data_expandible.classList.remove("active")
 }
 
 function ui_sostituzioni_filtra_data() {
-    ui_loading_sostituzioni()
-    filtri = sostituzioni_filtra_data()
-    s_richiedi_sostituzioni(filtri)
+  ui_loading_sostituzioni()
+  filtri = sostituzioni_filtra_data()
+  s_richiedi_sostituzioni(filtri)
 }
 
 function ui_sostituzioni_filtro_data_rimuovi_selected() {
-    ui_sostituzioni_filtro_data_oggi.classList.remove("selected")
-    ui_sostituzioni_filtro_data_domani.classList.remove("selected")
-    ui_sostituzioni_filtro_data_future.classList.remove("selected")
-    ui_sostituzioni_filtro_data_data.classList.remove("selected")
-    ui_sostituzioni_filtro_data_mese.classList.remove("selected")
-    ui_sostituzioni_filtro_data_tutte.classList.remove("selected")
+  ui_sostituzioni_filtro_data_oggi.classList.remove("selected")
+  ui_sostituzioni_filtro_data_domani.classList.remove("selected")
+  ui_sostituzioni_filtro_data_future.classList.remove("selected")
+  ui_sostituzioni_filtro_data_data.classList.remove("selected")
+  ui_sostituzioni_filtro_data_mese.classList.remove("selected")
+  ui_sostituzioni_filtro_data_tutte.classList.remove("selected")
 }
 
 ui_sostituzioni_filtro_data_oggi.onclick = (e) => {
-    sostituzioni_filtro_data_attivo = 'oggi'
-    ui_sostituzioni_filtra_data()
+  sostituzioni_filtro_data_attivo = 'oggi'
+  ui_sostituzioni_filtra_data()
 }
 
 ui_sostituzioni_filtro_data_domani.onclick = (e) => {
-    sostituzioni_filtro_data_attivo = 'domani'
-    ui_sostituzioni_filtra_data()
+  sostituzioni_filtro_data_attivo = 'domani'
+  ui_sostituzioni_filtra_data()
 }
 
 ui_sostituzioni_filtro_data_future.onclick = (e) => {
-    sostituzioni_filtro_data_attivo = 'future'
-    ui_sostituzioni_filtra_data()
+  sostituzioni_filtro_data_attivo = 'future'
+  ui_sostituzioni_filtra_data()
 }
 
 ui_sostituzioni_filtro_data_data.onclick =
-    ui_sostituzioni_filtro_data_data.onchange = (e) => {
-        sostituzioni_filtro_data_attivo = 'data'
-        ui_sostituzioni_filtra_data()
-    }
+  ui_sostituzioni_filtro_data_data.onchange = (e) => {
+    sostituzioni_filtro_data_attivo = 'data'
+    ui_sostituzioni_filtra_data()
+  }
 
 ui_sostituzioni_filtro_data_mese.onclick =
-    ui_sostituzioni_filtro_data_mese.onchange = (e) => {
-        sostituzioni_filtro_data_attivo = 'mese'
-        ui_sostituzioni_filtra_data()
-    }
+  ui_sostituzioni_filtro_data_mese.onchange = (e) => {
+    sostituzioni_filtro_data_attivo = 'mese'
+    ui_sostituzioni_filtra_data()
+  }
 
 ui_sostituzioni_filtro_data_tutte.onclick = (e) => {
-    sostituzioni_filtro_data_attivo = 'tutte'
-    ui_sostituzioni_filtra_data()
+  sostituzioni_filtro_data_attivo = 'tutte'
+  ui_sostituzioni_filtra_data()
 }
 
 ui_sostituzioni_filtro_data_data.valueAsDate = new Date()
