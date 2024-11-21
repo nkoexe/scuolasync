ef0d58bedf257c018333e16cb78723ba074af096
nkoexe <ask@njco.dev>
2024-11-21 10:22:20 +0100

    backslash again

--- scripts/install.sh
+++ scripts/install.sh
@@ -58,24 +58,24 @@ else
 fi
 
 # Define the Nginx configuration file content
-NGINX_CONF=$'
+NGINX_CONF='
 server {
         server_name '"$SERVER_NAME"';
 
         location / {
                 include proxy_params;
                 proxy_pass http://127.0.0.1:5123/;
-                proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-                proxy_set_header X-Forwarded-Proto $scheme;
-                proxy_set_header X-Forwarded-Host $host;
+                proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+                proxy_set_header X-Forwarded-Proto \$scheme;
+                proxy_set_header X-Forwarded-Host \$host;
         }
 
         location /socket.io {
                 include proxy_params;
                 proxy_http_version 1.1;
                 proxy_buffering off;
-                proxy_set_header Upgrade $http_upgrade;
-                proxy_set_header Connection "Upgrade";
+                proxy_set_header Upgrade \$http_upgrade;
+                proxy_set_header Connection \"Upgrade\";
                 proxy_pass http://127.0.0.1:5123/socket.io;
         }
 }
