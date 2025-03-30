a3cf1a6ee54f0f99700196fd0e36208f8ce3d863
nkoexe <niccolo.rag@gmail.com>
2025-03-30 20:31:48 +0200

    added popup event emitters

--- sostituzioni/view/static/scripts/lib/popup.js
+++ sostituzioni/view/static/scripts/lib/popup.js
@@ -2,6 +2,21 @@ class Popup {
   constructor({ query }) {
     this.element = document.querySelector(query);
     this.isOpen = false;
+
+    this.boundHandleOutsideClick = this.handleOutsideClick.bind(this);
+    this.boundHandleTouchStart = this.handleOutsideClick.bind(this); // Add this line
+    this.boundHandleEscapeKey = this.handleEscapeKey.bind(this);
+    
+    // Create event target for custom events
+    this.eventTarget = new EventTarget();
+  }
+
+  addEventListener(type, listener, options) {
+    this.eventTarget.addEventListener(type, listener, options);
+  }
+
+  removeEventListener(type, listener, options) {
+    this.eventTarget.removeEventListener(type, listener, options);
   }
 
   toggle() {
@@ -15,20 +30,33 @@ class Popup {
   open() {
     this.element.classList.add("open");
     this.isOpen = true;
-    document.addEventListener('click', this.handleOutsideClick.bind(this));
-    document.addEventListener('keydown', this.handleEscapeKey.bind(this));
+    document.addEventListener('keydown', this.boundHandleEscapeKey);
+
+    // Delay adding the click handler until the next event cycle
+    // This prevents the current click event from closing the popup
+    setTimeout(() => {
+      document.addEventListener('mousedown', this.boundHandleOutsideClick);
+      // Add touchstart for mobile device support
+      document.addEventListener('touchstart', this.boundHandleTouchStart);
+    }, 0);
+
+    this.eventTarget.dispatchEvent(new CustomEvent('open'));
   }
 
   close() {
     this.element.classList.remove("open");
     this.isOpen = false;
-    document.removeEventListener('click', this.handleOutsideClick.bind(this));
-    document.removeEventListener('keydown', this.handleEscapeKey.bind(this));
+    document.removeEventListener('mousedown', this.boundHandleOutsideClick);
+    document.removeEventListener('touchstart', this.boundHandleTouchStart);
+    document.removeEventListener('keydown', this.boundHandleEscapeKey);
+
+    this.eventTarget.dispatchEvent(new CustomEvent('close'));
   }
 
   handleOutsideClick(e) {
     if (!this.element.contains(e.target) &&
       e.target !== this.element) {
+      e.stopPropagation();
       this.close();
     }
   }
